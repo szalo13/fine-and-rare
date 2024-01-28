a220d2864af1e0f966dab0e05c8c92ebcac68d9c
Kamil Szalek <kamil@skyebrowse.com>
2024-01-28 11:26:02 +0100

    Install additional dependencies, add DB connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,18 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import connectDB from "./db";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const dbHost = process.env.DB_HOST || "localhost";
+const dbPort = process.env.DB_PORT || "27017";
+const dbName = process.env.DB_NAME || "find-and-rare";
+
+connectDB(dbHost, dbPort, dbName);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
